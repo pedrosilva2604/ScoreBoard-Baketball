8581fd86f885b4590669e400bb5bcee143950d48
pedrosilva2604 <pf17@hotmail.com>
2024-12-11 13:55:20 -0300

    functions BTN

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 let countHome = document.getElementById("count-home");
 let countGuest = document.getElementById("count-guest");
-let count = 0
+let countScoreHome = 0;
+let countScoreGuest = 0;
 
 function incrementHome() {
-    count += 1;
-    countHome.textContent = count;
+    countScoreHome += 1;
+    countHome.textContent = countScoreHome;
 };
 
 function incrementGuest() {
-    count += 1;
-    countGuest.textContent = count;
-}
+    countScoreGuest += 1;
+    countGuest.textContent = countScoreGuest;
+};
+
+function resetBtnHome() {
+    countHome.textContent = 0;
+    countScoreHome = 0;
+};
+
+function resetBtnGuest() {
+    countGuest.textContent = 0;
+    countScoreGuest = 0;
+};
+
+function incrementPlusTwoHome() {
+    countScoreHome += 2;
+    countHome.textContent = countScoreHome; 
+};
+
+function incrementPlusThreeHome() {
+    countScoreHome += 3;
+    countHome.textContent = countScoreHome;
+};
+
+function incrementPlusTwoGuest() {
+    countScoreGuest += 2;
+    countGuest.textContent = countScoreGuest;
+};
+
+function incrementPlusThreeGuest() {
+    countScoreGuest += 3;
+    countGuest.textContent = countScoreGuest;
+};
